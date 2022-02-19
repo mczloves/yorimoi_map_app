83254309d68ce9b63ea6c1033c3e480eb62fc501
mczloves <mczloves1@yahoo.co.jp>
2022-02-19 20:28:09 +0900

    いいね機能を実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,14 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
   with_options presence: true do
     validates :title
     validates :content
   end
+
+  def liked_by?(user)
+    likes.exists?(user_id: user.id)
+  end
+  
 end
