ce3e264c1fa14cbb093fd9a699633b80ceeab2a9
mczloves <mczloves1@yahoo.co.jp>
2022-02-21 23:33:59 +0900

    コメント機能の修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,21 +2,26 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
+    params[:post_id] = nil
     @comment = @post.comments.build(comment_params)
     @comment.user_id = current_user.id
-    @comment.save
-    redirect_to @post, notice: "コメントを書き込みました。"
+    if @comment.save
+      redirect_to @post, notice: "コメントを書き込みました。"
+    else
+      @post = Post.find(params[:post_id])
+      render :new
+    end
   end
 
   def destroy
     @post = Post.find(params[:post_id])
+    params[:post_id] = nil
     @comment = Comment.find(params[:id])
     @comment.destroy
     redirect_to @post, notice: "コメントを削除しました。"
   end
 
   private
-
   def comment_params
     params.require(:comment).permit(:body, :post_id, :user_id)
   end
