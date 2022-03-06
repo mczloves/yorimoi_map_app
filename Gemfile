368f49de0ee10146ca9ca4a99865d479df026dc7
mczloves <mczloves1@yahoo.co.jp>
2022-03-06 16:38:15 +0900

    管理者画面

--- Gemfile
+++ Gemfile
@@ -64,3 +64,4 @@ gem "devise-i18n"
 gem "mini_magick"
 gem "rails-i18n", "~> 6.0"
 gem "tzinfo-data", platforms: %i[mingw mswin x64_mingw jruby]
+gem 'activeadmin'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users
   root "pages#home"
-  get "maps/index"
   resources :posts do
     resource :likes, only: [:create, :destroy]
   end
