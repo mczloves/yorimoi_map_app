ccd1aed13d98a26379e1219133698cff8cbb0371
mczloves <mczloves1@yahoo.co.jp>
2022-03-07 17:09:46 +0900

    聖地リストを簡易的に作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :spots, only: [:index, :show]
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
   root "pages#home"
+  get "maps/index"
   resources :posts do
     resource :likes, only: [:create, :destroy]
   end
