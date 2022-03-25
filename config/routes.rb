20bbe0d672558356eea0448dfc118c2eff3d9236
mczloves <mczloves1@yahoo.co.jp>
2022-03-25 16:34:13 +0900

    ゲストログインを実装

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,15 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users
+  
+  devise_for :users, controllers: {
+    registrations: "users/registrations"
+  }
+
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
+
   root "pages#home"
   get "maps/index"
   resources :posts do
