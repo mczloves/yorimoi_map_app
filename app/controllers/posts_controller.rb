a2377bab83155d494522a4bb5de1ca388598b966
mczloves <mczloves1@yahoo.co.jp>
2022-03-27 00:16:42 +0900

    各ページのログイン設定を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_post, only: %i[edit update destroy]
 
   PER_PAGE = 12
